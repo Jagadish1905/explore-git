0775606ca27aa6c6f6283143588050cb10196b63
kotiroyal011 <kotiroyal1999@gmail.com>
2023-08-23 18:35:22 +0530

    increment by 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
-    return (
-        <div>
-            
-            <button onClick={() => setCount(count + 1)}>+</button>
-           <h1>{count}</h1> 
-            <button onClick={() => setCount(count - 1)}>-</button>
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 2)}>+3</button>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
