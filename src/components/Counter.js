7f6669cb76c3ffbede2fee24f71f763f5f373702
Jagadish <jagadish94bos@gmail.com>
2023-08-23 18:47:21 +0530

    added input field

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [isLogin, setLogin] = useState(false)
+    const [username, setUsername] = useState('')
     return (
         <div>
-            
+
             <button onClick={() => setCount(count + 1)}>+</button>
-           <h1>{count}</h1> 
+            <h1>{count}</h1>
             <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={setLogin(!isLogin)}>{isLogin ? "Login" : "Log out"}</button>
+            <input onChange={(e) => setUsername(e.target.value)}></input>
+            <p>{username}</p>
         </div>
     )
 }
