1084f208824fce39f4f91f762e1c6173c26f7616
madhu0311 <madhaviyadav7788@gmail.com>
2023-08-24 16:16:26 +0530

    create a new line

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
-    return (
-        <div>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            {count}
-            <button onClick={() => setCount(count - 1)}>-</button>
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      {count}
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <h1>this is madhavi</h1>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
