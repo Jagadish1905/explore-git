d48c4e1c4820674eec4ada0a1813ffd439dca9fa
Vikas8021 <105799165+Vikas8021@users.noreply.github.com>
2023-09-22 11:19:24 +0530

    This for the testing purpose

--- src/components/sign-in/Header.js
+++ src/components/sign-in/Header.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from "react";
 // import '../../assets/login.css'
 const Header = () => {
-    return (
-        <div>
-            <div className='header-container'>
+  return (
+    <div>
+      <div className="header-container">
+        <img
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt="logo"
+          width="200px"
+        ></img>
 
-                <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-                    alt="logo" width="200px"></img>
-
-                <div style={{ marginTop: "20px" }}>
-                    <select>
-                        <option>English</option>
-                        <option>Hindi</option>
-                        <option>Kannada</option>
-                        <option>Tamil</option>
-                    </select>
-                    <button >Sign In</button>
-                </div>
-            </div>
+        <div style={{ marginTop: "20px" }}>
+          <select>
+            <option>English</option>
+            <option>Hindi</option>
+            <option>Kannada</option>
+            <option>Tamil</option>
+            <option>Telugu</option>
+            <option>Marati</option>
+          </select>
+          <button>Sign In</button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
