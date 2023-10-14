3e73f0df6eaae7ef90b685eff08387e10e724fa4
Jagadish <jagadish94bos@gmail.com>
2023-10-14 14:06:46 +0530

    file3 file4 created

---
+++ src/components/File3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const File3 = () => {
+    return (
+        <div>File3</div>
+    )
+}
+
+export default File3
---
+++ src/components/File4.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const File4 = () => {
+    return (
+        <div>File4</div>
+    )
+}
+
+export default File4
