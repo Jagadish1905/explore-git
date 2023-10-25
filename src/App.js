8b6a918ee0bc5bb49db3c50163639270b9043f86
hanumaaa <hanumakola97@gmail.com>
2023-10-25 10:30:30 +0530

    create validation form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Hanuma from './component3/Hanuma';
-import File3 from './components/File3';
-import File1 from './components/File4';
-import Header from './components/sign-in/Header';
-import SignIn from './components/sign-in/SignIn';
-import GetButtonValue from './GetButtonValue';
-// import Counter from './components/Counter';
+import SearchPractice from './components/SearchPractice';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
-      {/* <Hanuma /> */}
-      <p>sdjklhflksfh</p>
-      <File1 />
+     <SearchPractice/>
 
     </div>
   );
